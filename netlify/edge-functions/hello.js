4f5c8f92ce5691c4b9fb9252d62d5252bd85403a
orderlyproblemsolvers <orderlyproblemsolvers@gmail.com>
2025-07-04 21:10:43 +0100

    fix: remove SERVER_PRESET to disable Edge Functions build

--- netlify/edge-functions/hello.js
+++
@@ -1,4 +0,0 @@
-// netlify/edge-functions/hello.js
-export default async (request, context) => {
-    return new Response("Hello from Edge!", { status: 200 });
-  };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr:true,
   nitro: {
-    preset: 'netlify'
+    preset: 'netlify-edge'
   },
   modules: ["@nuxt/ui", "@nuxt/image", '@nuxtjs/supabase'],
   image: {
